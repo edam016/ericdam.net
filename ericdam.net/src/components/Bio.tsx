815f188821623978fd238d3998317a41dc9fb770
Eric Dam <edam3268@gmail.com>
2023-04-07 02:41:25 -0400

    Editted About me page

--- ericdam.net/src/components/Bio.tsx
+++ ericdam.net/src/components/Bio.tsx
@@ -3,11 +3,12 @@ import './Bio.css';
 
 export const Bio = () => {
   return (
-    <div className="aboutme">
+    <div className="container">
         As an autodidactic, I believe that if I were to define myself through one all encompassing word, it would be a learner. 
         I am someone that believes in continous growth in a person in which this can come in many forms. From learning new skills to getting better and 
         better as a software engineer, its the process that really fascinates and interests me. And through this continous growth, it has continued to
         be the most exciting part of life.
+        <div className="aboutme"></div>
     </div>
   )
 }
